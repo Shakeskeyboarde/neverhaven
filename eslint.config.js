5cfd036e97bfe9b39a426a67db68cf715b0de945
Chris Ackerman <chris@topher.land>
2024-06-30 14:47:56 -0700

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,6 @@ import rational, { flatConfigBuilder } from 'eslint-config-rational';
 export default flatConfigBuilder()
   .ignore('**/{dist,lib,out,coverage,.terraform}')
   .use(rational, {
-    'tsDevFiles': (defaults) => [...defaults, '**/*.data.ts']
+    tsDevFiles: (defaults) => [...defaults, '**/*.data.ts'],
   })
-  .build()w
+  .build();
